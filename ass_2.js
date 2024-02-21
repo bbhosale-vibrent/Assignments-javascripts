941c6ccbd064c033124234f9b4772aa994f80f26
Bhagyashri Bhosale <bhagyashri.bhosale@vignet.com>
2024-02-21 13:39:29 +0530

    Done changes

--- ass_2.js
+++ ass_2.js
@@ -1,37 +1,29 @@
-function displayname(name)
-{
+function DisplayName(name) {
     let len = name.length;
     let result = `Length of the name ${name} : ${len}`;
-    // console.log('Length of the name is :',len);
-    console.log("Character in ordered list:");
-    console.log("<ol>");
-    for(let i=0;i<len;i++)
-    {
-        const letter = name[i];
-        const color = getcolor(letter);
-        // console.log('<li>'+name[i]+'</li>')
-        // console.log(`'<li>',+name[i]+'</li>`)
-        result += `\n <li> style="color:${color}">${letter}</li>`;
-    }
-    // console.log("</ol>");
-    result += `\n </ol>`;
     console.log(result);
-}
+    document.write(`<p>${result}</p>`);
+    document.write("<p> Character in ordered list:</p>");
+    document.write("<ol>");
+    let letter1 = "";
+    for (let i = 0; i < len; i++) {
+      const letter = name[i];
+      const color = getcolor(letter);
+      letter1 += ` <li style="color:${color}">${letter}</li>`;
+    }
+    letter1 += "</ul>";
+    document.write(letter1);
+  }
 
-function getcolor(letter)
-{
-    const color ={
-        'B': 'Blue',
-        'H' : 'None',
-        'A' : 'None',
-        'G' : 'Green',
-        'Y' : 'Yellow',
+  function getcolor(letter) {
+    const color = {
+      R: "Red",
+      O: "Orange",
+      Y: "Yellow",
+      P: "Purple",
     };
-    //  return color[letter];
-     return color[letter.toUpperCase()];
-
-}
-let name = "Bhagya";
-displayname(name);
-    
-
+    return color[letter.toUpperCase()];
+  }
+  let name = "rop";
+  DisplayName(name);
+  
