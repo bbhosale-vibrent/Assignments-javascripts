0444fa87b2cb4d6215efad9e8806929fabd21644
Bhagyashri Bhosale <156191606+bbhosale-vibrent@users.noreply.github.com>
2024-02-21 17:25:59 +0530

    Delete corsex.js

--- corsex.js
+++
@@ -1,7 +0,0 @@
-function gotWeather({ temperature, humidity }) {
-    alert(`temperature: ${temperature}, humidity: ${humidity}`);
-  }
-
-let script = document.createElement('script');
-script.src = `http://another.com/weather.json?callback=gotWeather`;
-document.body.append(script);
